6b063bd7dd94587618c43b48132ef6da53dca368
afterryz <root@WIN-D4N0G11I4NL.localdomain>
2026-02-21 21:16:21 +0600

    task 5

--- integral.c
+++ integral.c
@@ -1,16 +1,17 @@
 #include "integral.h"
 
 double calculate_integral(double (*func)(double), double a, double b, int n) {
-    if (n <= 0) return 0.0; 
+  if (n <= 0)
+    return 0.0;
 
-    double h = (b - a) / n;
-    
-    double sum = (func(a) + func(b)) / 2.0;
+  double h = (b - a) / n;
 
-    for (int i = 1; i < n; i++) {
-        double x_i = a + i * h;
-        sum += func(x_i);
-    }
+  double sum = (func(a) + func(b)) / 2.0;
 
-    return sum * h;
+  for (int i = 1; i < n; i++) {
+    double x_i = a + i * h;
+    sum += func(x_i);
+  }
+
+  return sum * h;
 }
--- main.c
+++ main.c
@@ -1,27 +1,25 @@
+#include "integral.h"
+#include <math.h>
 #include <stdio.h>
-#include <math.h>    
-#include "integral.h" 
 
-double test_function(double x) {
-    return x * x;
-}
+double test_function(double x) { return x * x; }
 
 int main() {
-    double a = 0.0;
-    double b = 1.0;
-    int n = 1000; 
+  double a = 0.0;
+  double b = 1.0;
+  int n = 1000;
 
-    double expected_value = 1.0/3.0;
-    double result = calculate_integral(test_function, a, b, n);
+  double expected_value = 1.0 / 3.0;
+  double result = calculate_integral(test_function, a, b, n);
 
-    printf("%.10f\n",result);
-    printf("%.10f\n",expected_value);
+  printf("%.10f\n", result);
+  printf("%.10f\n", expected_value);
 
-    double epsilon = 1e-6; 
-    if (fabs(result - expected_value) < epsilon) {
-        printf("НОРМ\n");
-    } else {
-        printf("НЕ НОРМ\n");
-    }
-    return 0;
+  double epsilon = 1e-6;
+  if (fabs(result - expected_value) < epsilon) {
+    printf("НОРМ\n");
+  } else {
+    printf("НЕ НОРМ\n");
+  }
+  return 0;
 }
