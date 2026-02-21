d5072c97084b502f384023b34911ffa0bdabd297
root <root@WIN-D4N0G11I4NL.localdomain>
2026-02-21 20:40:36 +0600

    task 5

---
+++ integral.c
@@ -0,0 +1,16 @@
+#include "integral.h"
+
+double calculate_integral(double (*func)(double), double a, double b, int n) {
+    if (n <= 0) return 0.0; 
+
+    double h = (b - a) / n;
+    
+    double sum = (func(a) + func(b)) / 2.0;
+
+    for (int i = 1; i < n; i++) {
+        double x_i = a + i * h;
+        sum += func(x_i);
+    }
+
+    return sum * h;
+}
